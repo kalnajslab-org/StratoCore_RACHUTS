cd39ab1b9d4fd269e2675ac13213d44b200ee875
Lars Kalnajs <kalnajslab@gmail.com>
2024-12-10 11:48:12 -0700

    Update for T4,1 and Rev E

--- PIBHardware.h
+++ PIBHardware.h
@@ -2,7 +2,8 @@
  *  PIBHardware.h
  *  Author:  Alex St. Clair
  *  Created: July 2019
- *
+ *  Updated for MonDo Board: November 2020
+ *  Updated for T4.1 Rev E Board: December 2024
  *  Pin and port definitions for the PIB
  */
 
@@ -11,22 +12,32 @@
 
 // Serial Ports
 #define DEBUG_SERIAL    Serial
-#define ZEPHYR_SERIAL   Serial
-#define MCB_SERIAL      Serial2
-#define PU_SERIAL       Serial3
+#define ZEPHYR_SERIAL   Serial1
+#define MCB_SERIAL      Serial3
+#define PU_SERIAL       Serial2
+#define RS41_SERIAL   Serial6
 
 // Digital Pins
-#define PU_PWR_ENABLE   26
-#define FORCEON_232     29
-#define FORCEOFF_232    30
+#define PU_PWR_ENABLE   2
+#define FORCEON_232     41 //Unused on MonDo and Rev E
+#define FORCEOFF_232    42 //Unused on MonDo and Rev E
 #define SAFE_PIN        31
 //#define MCB_IO_1        32
-#define PULSE_LED       38
+#define PULSE_LED       3
+#define RS41_PWR        36
 
 // Analog Pins
-#define VMON_3V3        A7
-#define VMON_12V        A20
+#define VMON_3V3        A16
+#define VMON_56V        A17
 #define VMON_15V        A2
-#define IMON_PU         A3
+#define IMON_PU         A8 //Profiler Current Monitor using 0.2 Ohm R on negative 
+#define IMON_PU_BTS     A9 //Profiler Current Monitor using BTS Current feedback
+#define IMON_MONDO      A3//Mondo current from current monitor chip
+
+// LoRa Module
+#define SS_PIN          38
+#define RESET_PIN       29
+#define INTERUPT_PIN    37
+
 
 #endif /* PIBHARDWARE_H */
